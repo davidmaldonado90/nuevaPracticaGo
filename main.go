5f54c2191c27d8e53c4fcb14e1894100e26b51e1
davidmaldonado90 <98559935+davidmaldonado90@users.noreply.github.com>
2026-01-04 11:08:48 -0300

    persistencia de datos en archivo txt

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func obtenerCategoria(Name string, Age int) string {
@@ -14,6 +15,21 @@ func obtenerCategoria(Name string, Age int) string {
 		return "Bienvenido junior"
 	}
 	return "Bienvenido Senior"
+}
+
+func guardarRegistro(texto string) {
+	f, err := os.OpenFile("log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("error al abrir archivo:", err)
+		return
+	}
+	defer f.Close()
+
+	_, err = f.WriteString(texto + "\n")
+
+	if err != nil {
+		fmt.Println("error al escribir:", err)
+	}
 }
 
 func main() {
@@ -37,8 +53,9 @@ func main() {
 			fmt.Scanln(&Age)
 
 			mensaje := obtenerCategoria(Name, Age)
-
 			fmt.Println(mensaje)
+
+			guardarRegistro("\n Nombre:" + Name + "Resultado: " + mensaje)
 
 		case 2:
 			fmt.Println("Saliendo del Sistema. Vuelva Prontos")
